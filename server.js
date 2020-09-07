2b7c531a67c137839c5c6aed229dc5c7e23580d0
Bhajanpreet Singh <bhajanpreet25@gmail.com>
2020-09-07 19:42:06 -0400

    added some spaces

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
+
 app.use(express.static(path.join(__dirname, "build")));
+
 app.get("/ping", function (req, res) {
   return res.send("pong");
 });
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 app.listen(port);
